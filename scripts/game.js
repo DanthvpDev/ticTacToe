ce23f0f74e46875dfd25aaebeac1acf27777ea6a
Daniel Hernández <danthdev02@gmail.com>
2025-01-16 18:35:13 -0600

    Rows verification finished

--- scripts/game.js
+++ scripts/game.js
@@ -1,9 +1,11 @@
+
+//* This change the turn validating if the turn corresponds to crosses (1) or circles (2)
 function ChangingTurns(turn) {
     return turn == 1 ? 0 : 1;
 }
 
 
-window.addEventListener('DOMContentLoaded', ()=> {
+window.addEventListener('DOMContentLoaded', () => {
     const StartGameButton = document.getElementById('startGameButton');
     const SymbolSelectionSection = document.getElementById('symbolSelection');
     const inputPlayerOneName = document.getElementById('playerOneName');
@@ -15,33 +17,35 @@ window.addEventListener('DOMContentLoaded', ()=> {
     const playerSymbolPicture = document.getElementById('playerSymbolPicture');
     const opponentSymbolPicture = document.getElementById('opponentSymbolPicture');
     const gridItems = document.querySelectorAll('.item-grid');
+    const boardRows = document.querySelectorAll('.board-row');
 
 
     let playerOne = new Player('Player1');
     let IsPVP;
     let playerTwo, computer;
     let turn;
+    let arr = []
 
 
-    StartGameButton.addEventListener('click', ()=> {
-        
+    StartGameButton.addEventListener('click', () => {
+
         //* This randomly assigns the symbol to the one who is having the turn 
         turn = AssigningSymbolRandomly();
 
 
         //* It validates if the Input for player name is filled out, if it is, it sets the name for player1
-        if(IsNameInputFilledOut(inputPlayerOneName.value)) playerOne.name = inputPlayerOneName.value;
+        if (IsNameInputFilledOut(inputPlayerOneName.value)) playerOne.name = inputPlayerOneName.value;
 
         //* This checks if player1 has not selected a symbol, so, it randomly assings the symbol
-        if(!IsSymbolSelected(playerOne)) playerOne.symbol = AssigningSymbolRandomly();
-        
+        if (!IsSymbolSelected(playerOne)) playerOne.symbol = AssigningSymbolRandomly();
+
 
         ShowingScoreSymbolPictures(playerOne, playerSymbolPicture);
 
         //* This validates  if the game mode is Players Vs Player or not to create the player two or the computer and showing the symbol on the score
-        if(IsPVP) {
+        if (IsPVP) {
             playerTwo = new Player('Player2');
-            if(IsNameInputFilledOut(inputPlayerTwoName.value)) playerTwo.name = inputPlayerTwoName.value;
+            if (IsNameInputFilledOut(inputPlayerTwoName.value)) playerTwo.name = inputPlayerTwoName.value;
             AssigningSymbolToOpponent(playerOne, playerTwo);
             ShowingScoreSymbolPictures(playerTwo, opponentSymbolPicture);
         }
@@ -59,51 +63,65 @@ window.addEventListener('DOMContentLoaded', ()=> {
     btnPvp.addEventListener('click', () => {
         IsPVP = true;
         //*This change the style of the selected element using a class to set the style rules.
-        if(!btnPvp.classList.contains('selected')) btnPvp.classList.toggle('selected');
+        if (!btnPvp.classList.contains('selected')) btnPvp.classList.toggle('selected');
 
         //* This verifies if the opposite button has the class, if it has it, this toggles the class. 
-        if(btnVsComputer.classList.contains('selected')) btnVsComputer.classList.toggle('selected');
+        if (btnVsComputer.classList.contains('selected')) btnVsComputer.classList.toggle('selected');
 
         //*This checks if the input for the name of player2 hasn't been displayed, if it isn't, this displays the element.
-        if(!inputPlayerTwoName.classList.contains('flex')) ShowingHiddingInputPlayerTwo(inputPlayerTwoName);
+        if (!inputPlayerTwoName.classList.contains('flex')) ShowingHiddingInputPlayerTwo(inputPlayerTwoName);
     })
 
-    btnVsComputer.addEventListener('click', ()=> {
+    btnVsComputer.addEventListener('click', () => {
         IsPVP = false;
-        if(btnPvp.classList.contains('selected')) btnPvp.classList.toggle('selected');
-        if(!btnVsComputer.classList.contains('selected')) btnVsComputer.classList.toggle('selected');
-        if(!inputPlayerTwoName.classList.contains('hidden')) ShowingHiddingInputPlayerTwo(inputPlayerTwoName);
+        if (btnPvp.classList.contains('selected')) btnPvp.classList.toggle('selected');
+        if (!btnVsComputer.classList.contains('selected')) btnVsComputer.classList.toggle('selected');
+        if (!inputPlayerTwoName.classList.contains('hidden')) ShowingHiddingInputPlayerTwo(inputPlayerTwoName);
     })
 
-    btnCircle.addEventListener('click', ()=> {
+    btnCircle.addEventListener('click', () => {
         playerOne.symbol = 0;
-        if(!btnCircle.classList.contains('selected')) btnCircle.classList.toggle('selected');
-        if(btnCross.classList.contains('selected')) btnCross.classList.toggle('selected');
+        if (!btnCircle.classList.contains('selected')) btnCircle.classList.toggle('selected');
+        if (btnCross.classList.contains('selected')) btnCross.classList.toggle('selected');
     })
 
-    btnCross.addEventListener('click', ()=> {
+    btnCross.addEventListener('click', () => {
         playerOne.symbol = 1;
-        if(!btnCross.classList.contains('selected')) btnCross.classList.toggle('selected');
-        if(btnCircle.classList.contains('selected')) btnCircle.classList.toggle('selected');
+        if (!btnCross.classList.contains('selected')) btnCross.classList.toggle('selected');
+        if (btnCircle.classList.contains('selected')) btnCircle.classList.toggle('selected');
     })
 
 
     gridItems.forEach(elem => {
-        elem.addEventListener('click', ()=> {
-            
+        elem.addEventListener('click', (e) => {
+            let targetParent = elem.parentNode;
+
             let pictureElement = document.createElement('picture');
             let imgElement = document.createElement('img');
             imgElement.className = 'w-9/12 h-full'
             pictureElement.className = 'w-full h-full flex justify-center';
 
+            if (!elem.children.length) {
+                if (turn == 1) {
+                    imgElement.src = '../media/img/Select=Cross.png';
+                    elem.name = 1;
 
-            turn == 1 
-                ? imgElement.src = '../media/img/Select=Cross.png'
-                : imgElement.src = '../media/img/Select=Circle.png'
-            
-            pictureElement.appendChild(imgElement);
+                }
+                else {
+                    imgElement.src = '../media/img/Select=Circle.png';
+                    elem.name = 0;
+                }
+                pictureElement.appendChild(imgElement);
 
-            elem.appendChild(pictureElement)
+                elem.appendChild(pictureElement);
+
+                turn = ChangingTurns(turn);
+            }
+
+
+            console.log(elem.name)
+            console.log(ValidatingWinnerRows(targetParent))
+
         })
     })
 
--- scripts/verifications.js
+++ scripts/verifications.js
@@ -6,3 +6,29 @@ function IsSymbolSelected(object) {
     return object.symbol != undefined || object.symbol != null ? true : false;
 }
 
+function IsGridFull(items) {
+    const numberGridFilled = Array.from(items).filter(item => item.children.length > 0).length;
+    return numberGridFilled == 9 ? true : false;
+}
+
+function ValidatingWinnerRows(element) {
+    
+    let arrayOfElements = Array.from(element.children)
+    
+    //*If 3 elements in a row have 1 as name, crosses won 
+    if (arrayOfElements.filter(el => el.name == 1).length == 3) return 1;
+    
+    //*If 3 elements in a row have 1 as name, circles won
+    if (arrayOfElements.filter(el => el.name == 0).length == 3) return 0;
+    
+    //* If there's no match returns null
+    return null;
+}
+
+
+function ValidatingWinnerColumns(element) {
+    //! let targetParentNextSibling = targetParent.nextElementSibling;
+    //! let targetParentPreviousSibling = targetParent.previousElementSibling;
+}
+
+
